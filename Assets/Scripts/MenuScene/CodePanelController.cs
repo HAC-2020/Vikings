5af1bbd2194092c0c4049f6c0074bd2c8fa3ba5c
MysteriousAcadia <anshumansingh64@gmail.com>
2020-08-08 14:33:55 +0530

    Add XR settings

--- Assets/Scripts/MenuScene/CodePanelController.cs
+++ Assets/Scripts/MenuScene/CodePanelController.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CodePanelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private float speed = 30f;
+    private Vector3 desiredScale ;
+    private bool close = false;
+    private void Awake()
     {
-        
+        transform.localScale = Vector3.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        desiredScale = Vector3.one;
+    }
+
+    public void Close()
+    {
+        desiredScale = Vector3.zero;
+        close = true;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, desiredScale, Time.deltaTime * speed);
+        if (transform.localScale == Vector3.zero && close)
+        {
+            close = false;
+            gameObject.SetActive(false);
+        }
     }
 }
