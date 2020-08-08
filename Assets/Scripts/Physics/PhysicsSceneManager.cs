570b5385e1f1e3558e9f9dfc23d96a28191cc014
MysteriousAcadia <anshumansingh64@gmail.com>
2020-08-09 01:58:06 +0530

    Modify electrical manager script

--- Assets/Scripts/Physics/PhysicsSceneManager.cs
+++ Assets/Scripts/Physics/PhysicsSceneManager.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class PhysicsSceneManager : MonoBehaviour
 {
     //STATES
-    // 1=put power
-    // 2=put rheostat
-    // 3 =put ammeter
+    // 1=put powerx
+    // 2=put rheostatx
+    // 3 =put ammeterx
+    // 4 =connect Power supply to rheostat
+    // 5 = connect rheostat to ammeter
+    // 6 = connect ammeter to power supply;
+    // 7 = Turn on power supply
+    // 8 = Run;
+
     public int state;
-    public GameObject rheostat, ammeter, power;
+    public GameObject rheostat, ammeter, power, c1, c2, c3;
+    public bool isRheostat, isAmmeter, isPower;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,14 +27,52 @@ public class PhysicsSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MakeConnections(GameObject g)
+    {
+        Debug.Log(g.name);
+        if (g.name == "rheostat")
+        {
+            if (state == 4 || state == 5)
+            {
+                isRheostat = true;
+            }
+
+        }
+        else if (g.name == "voltage_source")
+        {
+            if (state == 4 || state == 6)
+            {
+                isPower = true;
+            }
+        }
+        else if (g.name == "ammeter")
+        {
+            if (state == 6 || state == 5)
+            {
+                isAmmeter = true;
+            }
+        }
+        if ((state == 4 && isPower && isRheostat) ||
+        (state == 5 && isAmmeter && isRheostat) ||
+        (state == 6 && isPower && isAmmeter) ||
+        (state == 7 && isPower))
+        {
+            isAmmeter = false;
+            isPower = false;
+            isRheostat = false;
+            GoToNextState();
+        }
     }
     public void instantiateObjects(Button button)
     {
-        switch(button.name)
+        switch (button.name)
         {
             case "Power":
-                if(state==1){
+                if (state == 1)
+                {
                     power.SetActive(true);
                     GoToNextState();
                 }
@@ -66,6 +111,18 @@ public class PhysicsSceneManager : MonoBehaviour
     public void GoToNextState()
     {
         state++;
+        if (state == 5)
+        {
+            c1.SetActive(true);
+        }
+        else if (state == 6)
+        {
+            c2.SetActive(true);
+        }
+        else if (state == 7)
+        {
+            c3.SetActive(true);
+        }
     }
     public void WrongStep()
     {
