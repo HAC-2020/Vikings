d1605de61e24d4745e244dc816f45c2afe2f8083
MysteriousAcadia <anshumansingh64@gmail.com>
2020-08-09 14:59:45 +0530

    Add EchoAR and flash cards

---
+++ Assets/echoAR/SpawnHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class SpawnHandler : MonoBehaviour
+{
+    public Action destroyObject;
+    
+    public void DestroyObject()
+    {
+        destroyObject?.Invoke();
+    }
+    
+}
